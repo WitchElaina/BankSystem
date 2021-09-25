62fcb34694009b8b5a3f84a821a53b7b483361ba
LinYuanChan <msz13399173387@gmail.com>
2021-09-25 20:24:56 +0800

    Add codes

---
+++ register.cpp
@@ -0,0 +1,14 @@
+#include "register.h"
+#include "ui_register.h"
+
+Register::Register(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Register)
+{
+    ui->setupUi(this);
+}
+
+Register::~Register()
+{
+    delete ui;
+}
---
+++ register.h
@@ -0,0 +1,22 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <QDialog>
+
+namespace Ui {
+class Register;
+}
+
+class Register : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Register(QWidget *parent = nullptr);
+    ~Register();
+
+private:
+    Ui::Register *ui;
+};
+
+#endif // REGISTER_H
